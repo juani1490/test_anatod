fca6dac6b217efcff4a662d7e1204efc0475ebfc
Juan francisco Pell <juanfranciscopell@gmail.com>
2019-04-11 17:59:45 -0300

    edit-client.php fix

--- db/DataAccess/clients/edit-client.php
+++ db/DataAccess/clients/edit-client.php
@@ -3,7 +3,7 @@
   require dirname( dirname(__FILE__) ) . '/Entities/class.database.php';
 
   $db = new class_db();
-  $client = Client::Insert($_POST['Name'], $_POST['DNI'], $_POST['Location']);
+  $client = new Client($_POST['Name'], $_POST['DNI'], $_POST['Location']);
   $result = $db->Edit($_POST['Id'], $client);
 
   echo json_encode($client);
